0896a6fcc8cd9b53adf4e7dd98de43db94f84961
champuji <champujis10@gmail.com>
2021-12-05 16:21:32 +0530

    mUSIC

---
+++ handlers/pmpermit.py
@@ -0,0 +1,9 @@
+from callsmusic.callsmusic import client as USER
+from pyrogram import filters
+from pyrogram.types import Chat, Message, User
+
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+  await USER.send_message(message.chat.id,"Hello my name is Lucy x player I am play music on group first add lucy bot to your group and make admin and play songs")
+  return                        
